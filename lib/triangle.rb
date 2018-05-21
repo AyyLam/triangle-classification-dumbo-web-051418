0c8577b6e9fdc90cbc7fc7585c61ae8596075360
Anthony Lam <al3101@nyu.edu>
2018-05-21 18:19:38 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
-end
+  
+  def initialization(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2 
+    @length3 = length3
+  end
+  
+  def kind(triangle)
+    if @length1 == 0 || @length2 == 0 || @length3 == 0 
+      begin
+        raise TriangleError 
+      rescue TriangleError => error 
+        puts error.message
+      end 
+        
+    # if @length1 + @length2 <= @length3) || 
+    # @length2 + @length3 <= @length1||
+    # @length1 + @length3 <= @length2
+    #   begin
+    #     raise TriangleError 
+    #   rescue TriangleError => error 
+    #     puts error.message
+      
+    else
+      if @length1 == @length2 && @length3 == @length1 
+        return :equilaterial
+      elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3 
+        return :isosceles 
+
+      else 
+        return :scalene
+      end 
+    end 
+  end 
+  
+  class TriangleError < StandardError
+    def message 
+      "you must give the get_married method an argument of an instance of the person class!"
+    end 
+  end
+end 
